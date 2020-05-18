98f85fa39598bf669449c526063542a494dc32aa
vdsalex <alexandre.vandensteen@gmail.com>
2020-05-18 14:16:32 +0200

    add bordercard directive

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+  
+@Directive({
+  selector: '[pkmnBorderCard]'
+})
+export class BorderCardDirective {
+    constructor(private el: ElementRef) {
+        this.setBorder('#f5f5f5');
+        this.setHeight(180);
+    }
+
+    @HostListener('mouseenter') onMouseEnter() {
+        this.setBorder('#009688');
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.setBorder('#f5f5f5');
+    }
+  
+    private setBorder(color: string) {
+        let border = 'solid 4px ' + color;
+        this.el.nativeElement.style.border = border;
+    }
+  
+    private setHeight(height: number) {
+        this.el.nativeElement.style.height = height + 'px';
+    }
+}
