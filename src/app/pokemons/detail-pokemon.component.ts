87b97a9c54fd1bc28ab4de798db657d974b39788
vdsalex <alexandre.vandensteen@gmail.com>
2020-05-19 14:45:32 +0200

    update detail pokemon

--- src/app/pokemons/detail-pokemon.component.ts
+++ src/app/pokemons/detail-pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, Params } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pokemons';
