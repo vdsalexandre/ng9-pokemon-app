aa5fc7dfd3af28a08d9709d9b26dc169c38d9522
vdsalex <alexandre.vandensteen@gmail.com>
2020-05-20 16:04:08 +0200

    adding HttpClienModule to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PokemonsModule } from './pokemons/pokemons.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,7 @@ import { PageNotFoundComponent } from './page-not-found.component';
 @NgModule({
   imports: [  // classes nécessaires au fonctionnnement du module
     BrowserModule,    // l'ordre d'importation des modules est important !!
+    HttpClientModule,
     PokemonsModule, 
     AppRoutingModule 
   ],
